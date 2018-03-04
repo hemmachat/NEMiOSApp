f1198d436c0e79e40d3f01a974fbca7969680b32
Hemmachat Paramabuddhi <hemmachat@gmail.com>
2018-03-04 16:22:05 +1100

    Fix Swift 4 precedence syntax

--- NEMWallet/Assets/Supporting Files/Cryptography/RIPEMD-160/UInt32+CircularShift.swift
+++ NEMWallet/Assets/Supporting Files/Cryptography/RIPEMD-160/UInt32+CircularShift.swift
@@ -1,6 +1,11 @@
+precedencegroup BitwiseShiftLeftPrecedence {
+    associativity: none
+    higherThan: BitwiseShiftPrecedence
+}
 
-infix operator  ~<< { precedence 160 associativity none }
+infix operator ~<<: BitwiseShiftLeftPrecedence
 
 public func ~<< (lhs: UInt32, rhs: Int) -> UInt32 {
     return (lhs << UInt32(rhs)) | (lhs >> UInt32(32 - rhs));
 }
+
